c66ab67cb16c37981863cd8a2d64e73f6ebd7ea9
Josh <51213816+105hua@users.noreply.github.com>
2022-11-10 10:15:39 +0000

    First Upload.

---
+++ Car.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Car
+    {
+
+        public string carManufacturer { get; set; }
+        public string carModel { get; set; }
+        public string regPlate { get; set; }
+
+        public Car()
+        {
+
+            carManufacturer = "N/A";
+            carModel = "N/A";
+            regPlate = "N/A";
+
+        }
+
+        public Car(string manufacturer, string model, string regNo)
+        {
+
+            carManufacturer = manufacturer;
+            carModel = model;
+            regPlate = regNo;
+
+        }
+
+        public override string ToString()
+        {
+
+            return carManufacturer +
+                " - " + carModel +
+                " - " + regPlate;
+
+        }
+
+    }
+
+    class AdvancedCar : Car
+    {
+
+        public DateTime lastService { get; set; }
+        public DateTime lastMOT { get; set; }
+
+        public AdvancedCar() : base()
+        {
+
+            carManufacturer = "N/A";
+            carModel = "N/A";
+            regPlate = "N/A";
+            lastService = DateTime.Now;
+            lastMOT = DateTime.Now;
+
+        }
+
+        public AdvancedCar(string manufacturer, string model, string regNo, DateTime lastServiceDate, DateTime lastMOTDate) : base(manufacturer, model, regNo)
+        {
+
+            carManufacturer = manufacturer;
+            carModel = model;
+            regPlate = regNo;
+            lastService = lastServiceDate;
+            lastMOT = lastMOTDate;
+
+        }
+
+        public override string ToString()
+        {
+
+            return carManufacturer +
+                " - " + carModel +
+                " - " + regPlate +
+                " - " + lastService.ToShortDateString() +
+                " - " + lastMOT.ToShortDateString();
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Example;
+
+Car standardCar = new Car("Ford", "Focus", "OH58 LSK");
+AdvancedCar advancedCar = new AdvancedCar("Ford", "Focus", "OH58 LSK", DateTime.Parse("31/10/2022"), DateTime.Parse("29/10/2022"));
+Car blankStandardCar = new Car();
+AdvancedCar blankAdvancedCar = new AdvancedCar();
+
+Console.WriteLine(standardCar.ToString());
+Console.WriteLine(advancedCar.ToString());
+Console.WriteLine(blankStandardCar.ToString());
+Console.WriteLine(blankAdvancedCar.ToString());
